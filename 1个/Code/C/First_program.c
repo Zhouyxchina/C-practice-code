dc6b8d3ad767b9b265804b04db2defb51b0b8291
Zhouyxchina <65012658+Zhouyxchina@users.noreply.github.com>
2020-08-17 22:02:55 +0800

    Add files via upload

---
+++ 1个/Code/C/First_program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n");
+    printf("      ******************************************\n");
+    printf("      *                                        *\n");
+    printf("      *                                        *\n");
+    printf("      *   This is my first program!            *\n");
+    printf("      *                                        *\n");
+    printf("      *                                        *\n");
+    printf("      ******************************************\n");
+    printf("\n\n");
+
+    return 0;
+}
---
+++ 1个/Code/C/for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1,sum=0;
+
+    for (i = 1; i <= 5; ++i)
+    {
+        sum += i;
+    }
+    printf("\nsum =%d\n\n",sum);
+    
+    return 0;
+}
